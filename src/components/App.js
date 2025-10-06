342f223b29bfe311efc8439ff1b57a1178f786cf
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-10-06 20:48:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import UsersPage from './components/UsersPage';
 import NotificationsPage from './components/NotificationsPage';
@@ -17,30 +17,30 @@ function App() {
             <h1 className="nav-logo">SocialMedia</h1>
             <ul className="nav-menu">
               <li className="nav-item">
-                <NavLink to="/" className="nav-link">Home</NavLink>
+                <NavLink to="/" className="nav-link" activeClassName="active" exact>Home</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/users" className="nav-link">Users</NavLink>
+                <NavLink to="/users" className="nav-link" activeClassName="active">Users</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/notifications" className="nav-link">Notifications</NavLink>
+                <NavLink to="/notifications" className="nav-link" activeClassName="active">Notifications</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/create-post" className="nav-link">Create Post</NavLink>
+                <NavLink to="/create-post" className="nav-link" activeClassName="active">Create Post</NavLink>
               </li>
             </ul>
           </div>
         </nav>
 
         <main className="main-content">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/users" element={<UsersPage />} />
-            <Route path="/users/:userId" element={<UserPosts />} />
-            <Route path="/notifications" element={<NotificationsPage />} />
-            <Route path="/create-post" element={<CreatePost />} />
-            <Route path="/edit-post/:postId" element={<EditPost />} />
-          </Routes>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/users" component={UsersPage} />
+            <Route path="/users/:userId" component={UserPosts} />
+            <Route path="/notifications" component={NotificationsPage} />
+            <Route path="/create-post" component={CreatePost} />
+            <Route path="/edit-post/:postId" component={EditPost} />
+          </Switch>
         </main>
       </div>
     </Router>
